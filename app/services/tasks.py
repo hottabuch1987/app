477f27221f70aaa675fcb705b2d85baf3cb26971
hottabych <varvar1987a@mail.ru>
2023-05-08 11:03:18 +0300

    паралельные таски и transaction.atomic

--- app/services/tasks.py
+++ app/services/tasks.py
@@ -1,20 +1,47 @@
+import datetime
 import time
 
 from celery import shared_task
 from celery_singleton import Singleton
+from django.db import transaction
 from django.db.models import F
 
 
 @shared_task(base=Singleton)
 def set_price(subscription_id):
     from services.models import Subscription
+    #
+    print('do something 1')
+    with transaction.atomic():
 
-    time.sleep(5)
+        time.sleep(5)
 
-    subscription = Subscription.objects.filter(id=subscription_id).annotate(
-        annotated_price=F('service__full_price') -
-                    F('service__full_price') *
-                    F('plan__discount_percent') / 100.00).first()
+        subscription = Subscription.objects.select_for_update().filter(id=subscription_id).annotate(
+            annotated_price=F('service__full_price') -
+                        F('service__full_price') *
+                        F('plan__discount_percent') / 100.00).first()
 
-    subscription.price = subscription.annotated_price
-    subscription.save()
+        time.sleep(20)
+
+        subscription.price = subscription.annotated_price
+        subscription.save()
+    #
+    print('do something 1')
+
+@shared_task(base=Singleton)
+def set_comment(subscription_id):
+    from services.models import Subscription
+    #
+    print('do something 2')
+    with transaction.atomic():
+
+
+        subscription = Subscription.objects.select_for_update().get(id=subscription_id)
+
+        time.sleep(27)
+
+        subscription.comment = str(datetime.datetime.now())
+
+        subscription.save()
+    #
+    print('do something 2')
