215da9cb22c5bb0ff4fa898fac1147c0ac368f40
hottabych <varvar1987a@mail.ru>
2023-05-08 10:17:01 +0300

    пересчет на celery

--- app/services/models.py
+++ app/services/models.py
@@ -11,6 +11,17 @@ class Service(models.Model):
     def __str__(self):
         return f'{self.name}'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__full_price = self.full_price
+
+    def save(self, *args,  **kwargs):
+        if self.full_price != self.__full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
 
 class Plan(models.Model):
     PLAN_TYPES = (
@@ -26,6 +37,17 @@ class Plan(models.Model):
     def __str__(self):
         return f'{self.plan_type}'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__discount_percent = self.discount_percent
+
+    def save(self, *args,  **kwargs):
+        if self.discount_percent != self.__discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+
+        return super().save(*args, **kwargs)
+
 
 class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
@@ -33,9 +55,5 @@ class Subscription(models.Model):
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
     price = models.PositiveIntegerField(default=0)
 
-    def save(self, *args, save_model=True, **kwargs):
 
-        if save_model:
-            set_price.delay(self.id)
-        return super().save(*args, **kwargs)
 
